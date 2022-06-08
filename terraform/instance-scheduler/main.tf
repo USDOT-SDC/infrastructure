15d9399be9d250018f1da120780220914e21357f
Jeff U <Jeff.Ussing.ctr@dot.gov>
2022-06-08 18:22:36 +0000

    update lambda scr

--- terraform/instance-scheduler/main.tf
+++ terraform/instance-scheduler/main.tf
@@ -1,60 +1,60 @@
 
 locals {
-  environment           = "${data.aws_ssm_parameter.environment.value}"
+  environment = data.aws_ssm_parameter.environment.value
 
   tags = {
-    "Project"     = "SDC-Platform"
-    "Team"        = "SDC-Platform"
-    "Owner"       = "SDC Support Team"
+    "Project" = "SDC-Platform"
+    "Team"    = "SDC-Platform"
+    "Owner"   = "SDC Support Team"
   }
 }
 
-data "archive_file" "lambda_zip" {
-    type        = "zip"
-    source_dir  = "../src"
-    output_path = "instance-scheduler.zip"
+resource "null_resource" "copy_pytz" {
+  provisioner "file" {
+    source      = "${path.module}/lambda/venv/Lib/site-packages/pytz/"
+    destination = "${path.module}/lambda/src/pytz/"
+  }
 }
 
+resource "null_resource" "copy_yaml" {
+  provisioner "file" {
+    source      = "${path.module}/lambda/venv/Lib/site-packages/yaml/"
+    destination = "${path.module}/lambda/src/yaml/"
+  }
+}
 
 data "archive_file" "lambda" {
   type        = "zip"
+  source_dir  = "${path.module}/lambda/src/"
   output_path = "${path.module}/lambda/lambda.zip"
-
-  source {
-    content  = "${path.module}/lambda/src/schedule.py"
-    filename = "lambda_function.py"
-  }
-
-  source {
-    content  = "${path.module}/lambda/venv/Lib/site-packages/pytz/*.*"
-    filename = "pytz/*.*"
-  }
-
-  source {
-    content  = "${path.module}/lambda/venv/Lib/site-packages/yaml/*.*"
-    filename = "yaml/*.*"
-  }
+  depends_on = [
+    null_resource.copy_pytz,
+    null_resource.copy_yaml
+  ]
 }
 
 resource "aws_lambda_function" "instance-scheduler" {
-    filename = "instance-scheduler.zip"
-    function_name = "instance-scheduler"
-    role = aws_iam_role.instance_scheduler_role.arn
-    handler = "scheduler.lambda_handler"
-    timeout = 30
-    runtime = "python3.9"
-    tags = local.tags
+  filename      = "${path.module}/lambda/lambda.zip"
+  function_name = "instance-scheduler"
+  role          = aws_iam_role.instance_scheduler_role.arn
+  handler       = "scheduler.lambda_handler"
+  timeout       = 30
+  runtime       = "python3.9"
+  depends_on = [
+    data.archive_file.lambda
+  ]
+  tags = local.tags
 }
 
 resource "aws_iam_role" "instance_scheduler_role" {
-    name = "${local.environment}_instance_scheduler_role"
-    assume_role_policy = file("assume_role_policy.json")
-    tags = local.tags
+  name               = "${local.environment}_instance_scheduler_role"
+  assume_role_policy = file("assume_role_policy.json")
+  tags               = local.tags
 }
 
 resource "aws_iam_role_policy" "instance_scheduler_policy" {
-    name="${local.environment}_instance_scheduler_policy"
-    role = aws_iam_role.instance_scheduler_role.id
-    policy = file("instance_scheduler_policy.json")
-    tags = local.tags
+  name   = "${local.environment}_instance_scheduler_policy"
+  role   = aws_iam_role.instance_scheduler_role.id
+  policy = file("instance_scheduler_policy.json")
+  tags   = local.tags
 }
