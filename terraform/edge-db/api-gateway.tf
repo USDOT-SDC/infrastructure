1d73787cad25bcdd27eba84fdc84e21604ddb290
Gautam Naidu <gnaidu@avantusfederal.com>
2022-08-02 16:00:43 -0400

    Added lifecycle changes to api gateway

--- terraform/edge-db/api-gateway.tf
+++ terraform/edge-db/api-gateway.tf
@@ -12,12 +12,22 @@ data "aws_lambda_function" "webportal" {
 resource "aws_api_gateway_rest_api_policy" "webportal" {
   rest_api_id = var.common.webportal_api.id
   policy = file("${local.module}/policies/api-gateway_policy.json")
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
 resource "aws_api_gateway_resource" "exportTable" {
   rest_api_id = var.common.webportal_api.id
   parent_id   = var.common.webportal_api.root_resource_id
   path_part   = "exportTable"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
 resource "aws_api_gateway_method" "exportTable_OPTIONS" {
@@ -25,6 +35,11 @@ resource "aws_api_gateway_method" "exportTable_OPTIONS" {
     resource_id   = aws_api_gateway_resource.exportTable.id
     http_method   = "OPTIONS"
     authorization = "NONE"
+    lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
 resource "aws_api_gateway_method_response" "options_method_response_200" {
@@ -33,7 +48,7 @@ resource "aws_api_gateway_method_response" "options_method_response_200" {
     http_method   = aws_api_gateway_method.exportTable_OPTIONS.http_method
     status_code   = "200"
     response_models {
-        "application/json" = "Empty"
+      "application/json" = "Empty"
     }
     response_parameters {
         "method.response.header.Access-Control-Allow-Credentials" = true
@@ -43,7 +58,12 @@ resource "aws_api_gateway_method_response" "options_method_response_200" {
         "method.response.header.Access-Control-Expose-Headers" = true,
         "method.response.header.Access-Control-Max-Age" = true
     }
-    depends_on = ["aws_api_gateway_method.exportTable_OPTIONS"]
+    lifecycle {
+    ignore_changes = [
+      value,
+    ]
+    }
+  depends_on = ["aws_api_gateway_method.exportTable_OPTIONS"]
 }
 
 resource "aws_api_gateway_integration" "options_integration_request" {
@@ -51,6 +71,11 @@ resource "aws_api_gateway_integration" "options_integration_request" {
     resource_id   = aws_api_gateway_resource.exportTable.id
     http_method   = aws_api_gateway_method.exportTable_OPTIONS.http_method
     type          = "MOCK"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
     depends_on = ["aws_api_gateway_method.exportTable_OPTIONS"]
 }
 
@@ -67,6 +92,11 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
         "method.response.header.Access-Control-Expose-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Origin'",
         "method.response.header.Access-Control-Max-Age" = "'600'",
     }
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
     depends_on = ["aws_api_gateway_method_response.options_200"]
 }
 
@@ -79,6 +109,11 @@ resource "aws_api_gateway_method" "exportTable_POST" {
   request_parameters = {
     "method.request.path.proxy" = true
   }
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
 resource "aws_api_gateway_integration" "integration" {
@@ -88,5 +123,10 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.webportal.invoke_arn
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
