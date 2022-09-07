d1df7f9201f9513df2e060c85cbbd0933e2ba55c
Gautam Naidu <gnaidu@avantusfederal.com>
2022-09-07 13:56:35 -0400

    Updated Nightly Load Job

--- terraform/edge-db/jobs/glue.edge_db_nightly_load.py
+++ terraform/edge-db/jobs/glue.edge_db_nightly_load.py
@@ -1,3 +1,9 @@
+'''
+=============================================================================================================================
+Job Imports & Init
+=============================================================================================================================
+'''
+
 import sys
 import boto3
 
@@ -11,26 +17,32 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import mean as _mean, stddev as _stddev, col, length as length
 from pyspark.sql import *
 
-
-# dynamodb_resource = boto3.resource('dynamodb')
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+# Set Spark Configurations
 sc = SparkContext()
+sc.setLogLevel("DEBUG")
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 dynamodb = boto3.resource('dynamodb')
- 
+
+
+'''
+=============================================================================================================================
+Function Declarations
+=============================================================================================================================
+'''
 
 def scan_db(table, scan_kwargs=None):
     """
-    Get all records of the dynamodb table where the FilterExpression holds true
-    :param scan_kwargs: Used to pass filter conditions, know more about kwargs- geeksforgeeks.org/args-kwargs-python/
-    :type scan_kwargs: dict
-    :param table: dynamodb table name
-    :type table: str
-    :return: list of records
-    :rtype: dict
+    Overview:
+        Get all records of the dynamodb table where the FilterExpression holds true
+        
+    Function Details:
+        :param: scan_kwargs: dict: Used to pass filter conditions, know more about kwargs- geeksforgeeks.org/args-kwargs-python/
+        :param: table: string: Dynamodb table name
+        :return: records: dict: List of DynamoDB records returned.
     """
     if scan_kwargs is None:
         scan_kwargs = {}
@@ -50,6 +62,7 @@ def scan_db(table, scan_kwargs=None):
 
         complete = True if next_key is None else False
     return records
+
 
 def detect_schema_change(string1, string2):
     '''
@@ -106,18 +119,32 @@ def detect_schema_change(string1, string2):
             else:
                 return False #Return False if key doesn't exist in both schemas.
         return True
-        
+
+
 def qa_function(dyf_source, tableName, targetDBSchema, databaseName, stdDev_threshold=3):
+    """
+    Overview:
+        Iterates over all entries of a specified internal table and attempts to find outlier values, via Standard Deviation, when compared to data existing on the edge-clone table (the same table that exists on the edge side). This function doesn't reject any entry, but serves to warn Data Stewards if any entry seems unsual based on the previously published data.
+        
+    Function Details:
+        :param: dyf_source: Spark Dynamic Frame: A dynamic spark dataframe of the internal table (table containing potential outlier values). 
+        :param: tableName: string: The corressponding name of the internal table that 'dyf_source' is based on.
+        :param: targetDBSchema: string: Target Schema on the EdgeDB (schema in which the edge-clone table lives).
+        :param: databaseName: string: Name of the database in which the edge-clone table lives.
+        :optional_param: stdDev_threshold: string: The Standard Deviation threshold in which an entry is considered an outlier. Default set to 3 Standard Deviations.
+        :return: boolean, list: Returns a boolean value if outlier values were detected and a corressponding list of outlier values (if any).
+    """
     outlier_values = {}
     outlier_detection = False
     df_source = dyf_source.toDF()
     
+    # DEBUG/Testing Lines Below:
     # example_data = [('Bob', 'Smith', 1, 5.5),('Tom','Caesar',20, 20.6),('Steve','Patel',15, 43.66),('Moe','Shmo',14,37.44),('Justin','Fox', 5, 20.5), ('Kyle','Smith', 10, 26.7)] 
     # df_old = spark.createDataFrame(example_data, ['first_name','last_name','intCol', 'floatCol'])
 
     dyf_target = glueContext.create_dynamic_frame.from_catalog(database = "dev.sdc.dot.gov.research-team.data-export.edge", table_name = databaseName + "_" + targetDBSchema + "_" + tableName, transformation_ctx = "dyf_target")
     df_old = dyf_target.toDF()
-    
+
     column_list = df_source.columns
     for column in column_list:
         if str(df_old.schema[column].dataType) == 'StringType':
@@ -134,7 +161,7 @@ def qa_function(dyf_source, tableName, targetDBSchema, databaseName, stdDev_thre
             upperBound = mean + (std*stdDev_threshold)
             
             tempDF_new = df_source.withColumn(temp_column_name, length(column))
-            
+        
             outlierDF = tempDF_new.filter((tempDF_new[temp_column_name] < lowerBound) | (tempDF_new[temp_column_name] > upperBound))
             outlier_list = outlierDF.select(column).rdd.map(lambda x : x[0]).collect()
             if not outlier_detection:
@@ -162,8 +189,19 @@ def qa_function(dyf_source, tableName, targetDBSchema, databaseName, stdDev_thre
         else:
             continue
     return outlier_detection, outlier_values
+
         
 def send_notification(listOfRecipients, emailContent, subject = 'Export Notification: Schema Change Rejection'):
+    """
+    Overview:
+        A function to send out emails via AWS SES.
+        
+    Function Details:
+        :param: listOfRecipients: list of strings: A list of recipient email addresses.
+        :param: emailContent: string: The content of the email. HTML-formatted email passed in as a string.
+        :optional_param: subject: string: The title/subject of the email. Defaults to 'Schema Change Rejection'.
+        :return: None
+    """
     ses_client = boto3.client('ses')
     sender = 'sdc-support@dot.gov'
 
@@ -197,35 +235,50 @@ def send_notification(listOfRecipients, emailContent, subject = 'Export Notifica
     except Exception as e:
         print(e)
 
-# get list of tables from dynamodb where status = 'approved'
+
+'''
+=============================================================================================================================
+Main Script
+=============================================================================================================================
+'''
+
+''' -------------- DynamoDB Section --------------'''
 kwargs = {
-        'FilterExpression': Attr('RequestType').eq('table') & Attr('RequestReviewStatus').eq('Approved')
+        'FilterExpression': Attr('RequestType').eq('Table') & Attr('RequestReviewStatus').eq('Approved') # Filter Expression for DynamoDB Scan. Get entries where status = 'approved'
     }
+requests = scan_db('dev-RequestExportTable', kwargs) #Execute filter expression, defined in 'kwargs', on DynamoDB table 'dev-RequestExportTable'.
+print("Approved Export Requests: \n", requests) #Print Approved Export Requets in logs to show what entries have been looked at.
+
 # 1. check schema against schema attribute
-for request in scan_db('dev-RequestExportTable', kwargs):
+for request in requests:
     approved_schema = str(request['TableSchema'])
-    tableName = str(request["TableName"]).strip()
-    sourceSchema = str(request["SourceDatabaseSchema"]).strip()
-    targetSchema = str(request["TargetDatabaseSchema"]).strip()
-    databaseName = str(request["DatabaseName"]).strip()
+    tableName = str(request["TableName"])
+    tableName_glue = str(request["TableName"]).replace('-', '_').replace('.', '_').strip() #'.' and '-' characters have been replaced with '_' to comply with Glue Catalog needs.
+    sourceSchema_glue = str(request["SourceDatabaseSchema"]).replace('-', '_').replace('.', '_').strip()  #'.' and '-' characters have been replaced with '_' to comply with Glue Catalog needs.
+    targetSchema_glue = str(request["TargetDatabaseSchema"]).replace('-', '_').replace('.', '_').strip()  #'.' and '-' characters have been replaced with '_' to comply with Glue Catalog needs.
+    sourceSchema = str(request["SourceDatabaseSchema"]).strip()  #Used for direct JDBC connections (no glue catalog)
+    targetSchema = str(request["TargetDatabaseSchema"]).strip()  #Used for direct JDBC connections (no glue catalog)
+    databaseName_glue = str(request["DatabaseName"]).replace('-', '_').replace('.', '_').strip()  #'.' and '-' characters have been replaced with '_' to comply with Glue Catalog needs.
+    databaseName = str(request["DatabaseName"])
     userEmail = str(request["UserEmail"]).strip()
     listOfPOC = str(request["ListOfPOC"]).strip()
     
-    # create dynamic frame from approved table
-    datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "dev.sdc.dot.gov.research-team.data-export", table_name = databaseName + "_" + sourceSchema + "_" + tableName, transformation_ctx = "datasource0")
+    # Create dynamic frame based on the approved table table from DynamoDb record. Retrieves table from the internal database.
+    datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "dev.sdc.dot.gov.research-team.data-export", table_name = databaseName_glue + "_" + sourceSchema_glue + "_" + tableName_glue, transformation_ctx = "datasource0")
 
-    schema = datasource0._jdf.schema().treeString()
+    schema = datasource0._jdf.schema().treeString() #Isolates schema string of internal DB table for schema comparison based on initial/approved schema (part of approval record). 
     
-    print("schema: ", schema)
-    print("approved schema: ", approved_schema)
+    print("Internal Schema: ", schema) #Print Internal Table Schema to Logs.
+    print("Approved Schema: ", approved_schema) #Print Approved Table Schema to Logs.
+    print("No Change in Schema: ", detect_schema_change(schema, approved_schema)) #Print Schema Change Function Output to logs.
     
-    # if schema changes, resubmit for approval
+    # If schema has changed, Resubmit for approval, by updating previously approved DynamoDB record, to 'rejected' status.
     if not detect_schema_change(schema, approved_schema):
-        # email user of rejection
+        #Send an email to Users of Schema Rejection
         email_recipients = [userEmail]
         emailContent = "<br/>Export to EdgeDB has detected a schema change for the previously approved table: <b>" + tableName + "</b> for database <b>" + databaseName + ".</b> Please submit another export request to approve the new table schema at https://portal.sdc.dot.gov.<b>" + "</b>"
         send_notification(email_recipients,emailContent)
-        # update dynamodb entry to status: "rejected"
+        # Update corressponding Dynamodb entry to status: "rejected"
         exportRequests_table = dynamodb.Table('dev-RequestExportTable')
         response = exportRequests_table.update_item(
             Key={
@@ -234,30 +287,33 @@ for request in scan_db('dev-RequestExportTable', kwargs):
             },
             UpdateExpression="set RequestReviewStatus = :r",
             ExpressionAttributeValues={
-                ':r': 'Rejected',
+                ':r': 'Rejected'
             }
         )
-        continue
 
-    # e-mail approvers any outlier values detected
-    try:
-        outlier_flag, outlier_values = qa_function(datasource0, tableName, targetSchema, databaseName)
-    except Exception as e:
-        print("Unable to determine target outlier values. Function failed with: ", e)
     else:
-        if outlier_flag:
+        try:
+            outlier_flag, outlier_values = qa_function(datasource0, tableName_glue, targetSchema_glue, databaseName_glue)
+        except Exception as e:
+            print("Unable to determine target outlier values. Function failed with: ", e)
+            outlier_flag = False
+            pass
+        if outlier_flag:         
+            #Send an email to approvers of outlier values detected. QA Email.
             email_recipients = [listOfPOC]
             emailContent = "<br/>Export to EdgeDB has detected the following values significantly higher or lower (longer or shorter) than average for table: <b>" + tableName + "</b> for database <b>" + databaseName + ":</b>" + str(outlier_values) + " Please notify sdc-support@dot.gov if these values are not acceptable.<b>" + "</b>"
             emailSubject = 'Export Notification: Anomalies Detected'
             send_notification(email_recipients,emailContent, emailSubject)
             print(outlier_values)
         
-    # write table to edge db
+    # write internal table dynamic frame to EdgeDB (overwrite edge clone table with new data). Overwrites table to handle any existing entries that were modified.
     dbtable = targetSchema + "." + tableName
     # datasink1 = glueContext.write_dynamic_frame.from_jdbc_conf(frame = datasource0, catalog_connection = "Data Export Edge Acme DB", connection_options = {"dbtable": dbtable, "database": databaseName}, transformation_ctx = "datasink1")
 
     jdbcDF = datasource0.toDF()
-
+    print(f'Dataframe to be written {jdbcDF.head()}')
+    #JDBC Connection to EdgeDB
+    #TODO: Get Admin Credentials into SSM Parameter Store.
     x = jdbcDF.write.format("jdbc") \
     .option("url", f"jdbc:postgresql://aurora-dataexport-edge-instance-1.cbldpsthn7bv.us-east-1.rds.amazonaws.com:5432/" + databaseName) \
     .option("dbtable", dbtable) \
