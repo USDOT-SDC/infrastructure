a14172e904742d15f69d9991f106660bfde696dc
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2022-12-01 21:24:27 +0000

    add support and infrastructure subnets

--- terraform/vpc/subnets.tf
+++ terraform/vpc/subnets.tf
@@ -15,10 +15,27 @@ resource "aws_subnet" "routing" {
   }
 }
 
+resource "aws_subnet" "support" {
+  vpc_id     = var.common.network.vpc.id
+  cidr_block = local.subnet_support.cidr_block
+  tags = {
+    Name = local.subnet_support.name
+  }
+}
+
 resource "aws_subnet" "researcher" {
   vpc_id     = var.common.network.vpc.id
   cidr_block = local.subnet_researcher.cidr_block
   tags = {
     Name = local.subnet_researcher.name
+  }
+}
+
+resource "aws_subnet" "infrastructure" {
+  count      = length(local.subnets_infrastructure)
+  vpc_id     = var.common.network.vpc.id
+  cidr_block = local.subnets_infrastructure[count.index].cidr_block
+  tags = {
+    Name = local.subnets_infrastructure[count.index].name
   }
 }
