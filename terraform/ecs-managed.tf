9e1565703635a142629972d71dea541fc370bab7
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2022-12-01 04:16:38 +0000

    add transit_gateway data

--- terraform/ecs-managed.tf
+++ terraform/ecs-managed.tf
@@ -81,3 +81,10 @@ data "aws_security_group" "default" {
   vpc_id = data.aws_vpc.public.id
   name = "default"
 }
+
+data "aws_ec2_transit_gateway" "default" {
+  filter {
+    name   = "tag:Name"
+    values = ["Default"]
+  }
+}
