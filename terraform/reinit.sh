0b6c53fbf9a29d2e42ab0adc2c270bfc9b78d0f2
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2023-05-09 22:57:55 -0400

    remove Linux bash scripts

--- terraform/reinit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Use this bucket for backend:"
-echo ${AWS_ENVIRONMENT}.sdc.dot.gov.platform.terraform
-
-terraform init
-
--- terraform/utilities/log4sdc/reapply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG4SDC_MODULE=module.utilities.module.log4sdc
-
-##################
-# log4sdc
-terraform apply -target=${LOG4SDC_MODULE}
-
-
