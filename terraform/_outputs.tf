b01083da8c539e0fe1bcdf308a16c5161ce64e52
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2023-05-16 19:29:13 -0400

    add local.common to outputs

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -1,3 +1,9 @@
+# use caution when making changes to local.common
+# local.common is output to tfstate and used by other configurations
+output "common" {
+    value = local.common
+}
+
 output "vpc_debug" {
     value = module.vpc.debug
 }
