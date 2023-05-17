c78499078bc83d9ed706497681fdc26746cbf73f
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2023-05-16 22:17:30 -0400

    add output for disk_alert_linux_script

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -1,5 +1,8 @@
 # use caution when making changes to local.common
 # local.common is output to tfstate and used by other configurations
 output "common" {
-    value = local.common
+  value = local.common
+}
+output "disk_alert_linux_script" {
+  value = module.utilities.disk_alert_linux_script
 }
--- terraform/utilities/disk-alert-linux.tf
+++ terraform/utilities/disk-alert-linux.tf
@@ -3,7 +3,7 @@ locals {
   disk_alert_linux_script_path = "${basename(abspath(path.module))}/${local.disk_alert_linux_script}"
 }
 
-resource "aws_s3_object" "disk_alert_linux" {
+resource "aws_s3_object" "disk_alert_linux_script" {
   bucket = var.common.instance_maintenance_bucket.id
   key    = "infrastructure/${local.disk_alert_linux_script_path}"
   content = templatefile(
@@ -13,4 +13,11 @@ resource "aws_s3_object" "disk_alert_linux" {
       email_address = var.common.admin_email
     }
   )
+}
+
+output "disk_alert_linux_script" {
+  value = {
+    bucket = aws_s3_object.disk_alert_linux_script.bucket,
+    key    = aws_s3_object.disk_alert_linux_script.key
+  }
 }
