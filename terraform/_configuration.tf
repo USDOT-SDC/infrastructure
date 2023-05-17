bc3797012aa8d98e788457a37b43e4ba09a48e64
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2023-05-16 21:41:41 -0400

    formating

--- terraform/_configuration.tf
+++ terraform/_configuration.tf
@@ -2,26 +2,26 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "~> 2.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "~> 2.0"
     }
   }
   backend "s3" {
     # Variables can not be used here
     region = "us-east-1"
-    key = "infrastructure/terraform/terraform.tfstate"
+    key    = "infrastructure/terraform/terraform.tfstate"
   }
 }
 
