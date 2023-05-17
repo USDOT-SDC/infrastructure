b1f591ffd8172be17f5e2b363f8537947f492343
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2023-05-16 21:40:58 -0400

    add terraform_remote_state.research_teams

--- terraform/_variables.tf
+++ terraform/_variables.tf
@@ -22,11 +22,12 @@ locals {
       default_security_group = data.aws_security_group.default
       transit_gateway        = data.aws_ec2_transit_gateway.default
     }
-    support_email               = nonsensitive(data.aws_ssm_parameter.support_email.value)
-    admin_email                 = nonsensitive(data.aws_ssm_parameter.admin_email.value)
-    terraform_bucket            = aws_s3_bucket.terraform
-    backup_bucket               = aws_s3_bucket.backup
-    instance_maintenance_bucket = aws_s3_bucket.instance_maintenance
+    support_email                      = nonsensitive(data.aws_ssm_parameter.support_email.value)
+    admin_email                        = nonsensitive(data.aws_ssm_parameter.admin_email.value)
+    terraform_bucket                   = aws_s3_bucket.terraform
+    backup_bucket                      = aws_s3_bucket.backup
+    instance_maintenance_bucket        = aws_s3_bucket.instance_maintenance
+    research_teams_vpc_endpoint_lambda = data.terraform_remote_state.research_teams.outputs.vpc_endpoint_lambda.dns_entry[0].dns_name
   }
   default_tags = {
     "Repository URL" = "https://github.com/USDOT-SDC/"
@@ -35,5 +36,4 @@ locals {
     Team             = "SDC-Platform"
     Owner            = "SDC Support Team"
   }
-  provider-profile = "sdc"
 }
