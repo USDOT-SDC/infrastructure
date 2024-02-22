55fe53384906362667f4a476e1a45d0f89688d1a
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-02-22 14:21:47 -0500

    update provider version constraints

--- terraform/_configuration.tf
+++ terraform/_configuration.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.36"
+      version = "~> 5.37"
     }
     null = {
       source  = "hashicorp/null"
