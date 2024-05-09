ca6ec1c6853873dd2375cb0ae7f02439f020348b
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-05-09 19:30:56 -0400

    remove s3_interface

--- terraform/vpc/endpoints.tf
+++ terraform/vpc/endpoints.tf
@@ -42,53 +42,6 @@ resource "aws_vpc_endpoint_route_table_association" "s3" {
   vpc_endpoint_id = aws_vpc_endpoint.s3_gateway.id
 }
 
-# S3 Interface
-resource "aws_vpc_endpoint" "s3_interface" {
-  vpc_id            = var.common.network.vpc.id
-  vpc_endpoint_type = "Interface"
-  service_name      = "com.amazonaws.${var.common.region}.s3"
-  subnet_ids = concat(
-    [
-      aws_subnet.support.id,
-      aws_subnet.researcher.id
-    ],
-    aws_subnet.infrastructure[*].id
-  )
-  tags = {
-    "Name" = "S3 Interface"
-  }
-}
-
-resource "aws_vpc_endpoint_policy" "s3_interface" {
-  vpc_endpoint_id = aws_vpc_endpoint.s3_interface.id
-  policy = jsonencode(
-    {
-      "Version" : "2008-10-17",
-      "Statement" : [
-        {
-          "Sid" : "AllowAll",
-          "Effect" : "Allow",
-          "Principal" : "*",
-          "Action" : "*",
-          "Resource" : "*"
-        },
-        {
-          "Sid" : "EnforceTLSv12orHigher",
-          "Effect" : "Deny",
-          "Principal" : "*",
-          "Action" : "*",
-          "Resource" : "*",
-          "Condition" : {
-            "NumericLessThan" : {
-              "s3:TlsVersion" : 1.2
-            }
-          }
-        }
-      ]
-    }
-  )
-}
-
 # SSM Interface
 resource "aws_vpc_endpoint" "ssm_interface" {
   vpc_id            = var.common.network.vpc.id
