e5247646b2de86363288816636ed55c8952fcfa7
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-05-17 19:13:22 -0400

    add type hints

--- terraform/api/token/src/lambda_function.py
+++ terraform/api/token/src/lambda_function.py
@@ -1,79 +1,90 @@
 import json
 import sys
-import boto3
-# from boto3 import client
-import datetime
+from typing import Any
+from boto3 import client
 import uuid
 
 
-def iam_client():
-  return boto3.client('iam')
+def iam_client() -> client:
+    return client("iam")
 
-def get_tag_value_from_key(key, tags):
-  for tag in tags:
-    if tag.get("Key", False) == key:
-      return tag.get("Value", False)
 
-def sts_client():
-  return boto3.client('sts')
+def get_tag_value_from_key(key: str, tags: list[dict]) -> str | bool:
+    for tag in tags:
+        if tag.get("Key", False) == key:
+            return tag.get("Value", False)
+    return False
 
-def json_converter(o):
-  if isinstance(o, datetime.datetime):
-    return o.__str__()
 
-def get_uuid():
-  return str(uuid.uuid4())
+def int_from_str(string: str) -> int | bool:
+    try:
+        return int(string)
+    except ValueError as e:
+        print(f"{string} cannot be converted to an int: {e}")
+        return False
 
-def lambda_handler(event, context):
-    
+
+def sts_client() -> client:
+    return client("sts")
+
+
+def get_uuid() -> str:
+    return str(uuid.uuid4())
+
+
+def lambda_handler(event: dict, context: dict) -> dict[str, Any]:
+
     # check that username was passed
-    username = event.get("queryStringParameters", False).get("username", False)
+    username: str | bool = event.get("queryStringParameters", False).get("username", False)
     if not username:
         sys.exit("No username was found in the queryStringParameters")
     print(f"Username: {username}")
-    
+
     # check that pin was passed
-    user_pin = event.get("queryStringParameters", False).get("pin", False)
+    user_pin: str | bool = event.get("queryStringParameters", False).get("pin", False)
     if not user_pin:
         sys.exit("No pin was found in the queryStringParameters")
     print(f"User PIN: {user_pin}")
-    
-    
+
     # get the pin from the role
-    role_name = f"user_{username}"
-    response = iam_client().list_role_tags(RoleName=role_name)
-    tags = response.get("Tags", False)
+    role_name: str = f"user_{username}"
+    response: dict = iam_client().list_role_tags(RoleName=role_name)
+    tags: list | bool = response.get("Tags", False)
     if not tags:
         sys.exit("No tags were found on the IAM Role")
-    role_pin = get_tag_value_from_key("pin", tags)
+    role_pin: str = get_tag_value_from_key("pin", tags)
     if not role_pin:
         sys.exit("No tag:pin was found on the IAM Role")
-    
+
+    # check that both pins are int-able
+    user_pin = int_from_str(user_pin)
+    if not user_pin:
+        sys.exit("The user PIN was not int-able")
+    role_pin = int_from_str(role_pin)
+    if not role_pin:
+        sys.exit("The role PIN was not int-able")
+
     # check the user pin against the role pin
     if user_pin != role_pin:
         sys.exit("Incorrect PIN")
 
     # get the account id, role arn and uuid
-    account_id = sts_client().get_caller_identity()['Account']
-    role_arn= f"arn:aws:iam::{account_id}:role/{role_name}" 
-    unique_id = get_uuid()
+    account_id: str = sts_client().get_caller_identity().get("Account", "")
+    role_arn: str = f"arn:aws:iam::{account_id}:role/{role_name}"
+    unique_id: str = get_uuid()
 
     # assume the role and get the assumed_role
-    assumed_role = sts_client().assume_role(
-        RoleArn=role_arn,
-        RoleSessionName=unique_id,
-        DurationSeconds=3600
-    )
-    
-    # get the credentials from the assumed_role and make them json
-    credentials = assumed_role['Credentials']
-    credentials = json.dumps(credentials, default=json_converter)
+    assumed_role: dict = sts_client().assume_role(RoleArn=role_arn, RoleSessionName=unique_id, DurationSeconds=3600)
+
+    # get the credentials from the assumed_role and make them json str
+    credentials: dict = assumed_role.get("Credentials", {})
+    credentials: str = json.dumps(credentials, indent=3, default=str)
     print("credentials: " + credentials)
 
-    # return the credentials as 
+    # return the credentials
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
         "body": credentials,
-        "isBase64Encoded": False
+        "isBase64Encoded": False,
     }
