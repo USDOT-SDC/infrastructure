323a765fd5f3644b2529da19d7dc3dbccff47515
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-05-17 19:14:56 -0400

    update name and disc

--- terraform/api/token_lambda.tf
+++ terraform/api/token_lambda.tf
@@ -5,8 +5,8 @@ data "archive_file" "token_lambda_deployment_package" {
 }
 
 resource "aws_lambda_function" "token" {
-  function_name    = "api_generate_token"
-  description      = "API Function to refresh an IAM role's sesstion token"
+  function_name    = "api_token"
+  description      = "Function to refresh an IAM role's sesstion token via API resource"
   filename         = data.archive_file.token_lambda_deployment_package.output_path
   source_code_hash = data.archive_file.token_lambda_deployment_package.output_base64sha256
   role             = "arn:aws:iam::505135622787:role/service-role/api_generate_token"
