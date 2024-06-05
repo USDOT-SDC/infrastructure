635bf1c5b2872b7715f233bcd0e84d8599192e53
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-06-05 16:11:42 -0400

    add items for schema examples

--- terraform/auto_start/dynamodb.tf
+++ terraform/auto_start/dynamodb.tf
@@ -11,6 +11,35 @@ resource "aws_dynamodb_table" "auto_starts" {
   tags = local.tags
 }
 
+resource "aws_dynamodb_table_item" "auto_start_example" {
+  table_name = aws_dynamodb_table.auto_starts.name
+  hash_key   = aws_dynamodb_table.auto_starts.hash_key
+
+  item = jsonencode(
+    {
+      "instance_id" : {
+        "S" : "i-0daca5caa7c550bab_example"
+      },
+      "cron_expressions" : {
+        "L" : [
+          {
+            "S" : "55 * * * *"
+          },
+          {
+            "S" : "5 * * * *"
+          }
+        ]
+      },
+      "name" : {
+        "S" : "ECSDWART01_SchemaExample"
+      },
+      "timezone" : {
+        "S" : "EST"
+      }
+    }
+  )
+}
+
 resource "aws_dynamodb_table" "maintenance_windows" {
   name         = "instance_maintenance_windows"
   billing_mode = "PAY_PER_REQUEST"
@@ -22,4 +51,27 @@ resource "aws_dynamodb_table" "maintenance_windows" {
   }
 
   tags = local.tags
+}
+
+
+resource "aws_dynamodb_table_item" "maintenance_windows_example" {
+  table_name = aws_dynamodb_table.maintenance_windows.name
+  hash_key   = aws_dynamodb_table.maintenance_windows.hash_key
+
+  item = jsonencode(
+    {
+      "maintenance_window_id" : {
+        "S" : "Tuesday09_SchemaExample"
+      },
+      "cron_expression" : {
+        "S" : "0 9 * * 2"
+      },
+      "duration" : {
+        "S" : "8:00"
+      },
+      "timezone" : {
+        "S" : "EST"
+      }
+    }
+  )
 }
