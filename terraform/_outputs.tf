f54b5f11cf075672df346161be37d3b322b3d20c
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-06-06 22:39:22 -0400

    remove deprecated output of common var

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -1,35 +1,5 @@
-# use caution when making changes to local.common
-# local.common is output to tfstate and used by other configurations
-# the common output is deprecated. use vpc or s3 outputs, or read directly from the ssm parameters
-output "common" {
-  value = {
-    account_id  = nonsensitive(data.aws_ssm_parameter.account_id.value)
-    region      = nonsensitive(data.aws_ssm_parameter.region.value)
-    environment = nonsensitive(data.aws_ssm_parameter.environment.value)
-    network = {
-      vpc = { id = data.aws_vpc.default.id }
-      subnets = concat(
-        [module.vpc.subnet_support.id],
-        [module.vpc.subnet_researcher.id],
-        tolist(module.vpc.subnets_infrastructure[*].id),
-      )
-      subnet_support         = { id = module.vpc.subnet_support.id }
-      subnet_researcher      = { id = module.vpc.subnet_researcher.id }
-      subnet_three           = { id = module.vpc.subnets_infrastructure[0].id }
-      subnet_four            = { id = module.vpc.subnets_infrastructure[1].id }
-      subnet_five            = { id = module.vpc.subnets_infrastructure[2].id }
-      subnet_six             = { id = module.vpc.subnets_infrastructure[3].id }
-      default_security_group = { id = data.aws_security_group.default.id }
-      transit_gateway        = { id = data.aws_ec2_transit_gateway.default.id }
-    }
-    support_email               = nonsensitive(data.aws_ssm_parameter.support_email.value)
-    admin_email                 = nonsensitive(data.aws_ssm_parameter.admin_email.value)
-    terraform_bucket            = { id = aws_s3_bucket.terraform.id }
-    backup_bucket               = { id = aws_s3_bucket.backup.id }
-    instance_maintenance_bucket = { id = aws_s3_bucket.instance_maintenance.id }
-  }
-}
-
+# use caution when making changes to outputs
+# these are put into the tfstate file and used by other Terraform configurations
 output "vpc" {
   value = {
     id = data.aws_vpc.default.id
