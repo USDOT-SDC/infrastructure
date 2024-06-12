22c8d70fe24f83384e941a146c597b6b0442b4af
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-06-12 14:03:23 -0400

    import route53 and certs from portal2 config

--- terraform/import.tf
+++ terraform/import.tf
@@ -2,3 +2,23 @@
 #   to = module.api.
 #   id = ""
 # }
+
+# import {
+#   to = aws_route53_zone.public
+#   id = "Z103672221FNFH7O9E9OG"
+# }
+
+# import {
+#   to = aws_route53_record.ns
+#   id = "Z103672221FNFH7O9E9OG_sdc-dev.dot.gov_NS"
+# }
+
+# import {
+#   to = aws_route53_record.soa
+#   id = "Z103672221FNFH7O9E9OG_sdc-dev.dot.gov_SOA"
+# }
+
+# import {
+#   to = aws_acm_certificate.external
+#   id = "arn:aws:acm:us-east-1:505135622787:certificate/0dadb63d-8898-4260-ac16-545a2f0c4999"
+# }
