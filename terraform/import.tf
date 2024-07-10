7cb45185386bad805e643e75315952241e687fb3
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-07-10 19:34:26 -0400

    import private route53 resources

--- terraform/import.tf
+++ terraform/import.tf
@@ -9,13 +9,33 @@
 # }
 
 # import {
-#   to = aws_route53_record.ns
+#   to = aws_route53_record.public_ns
 #   id = "Z103672221FNFH7O9E9OG_sdc-dev.dot.gov_NS"
 # }
 
 # import {
-#   to = aws_route53_record.soa
+#   to = aws_route53_record.public_soa
 #   id = "Z103672221FNFH7O9E9OG_sdc-dev.dot.gov_SOA"
+# }
+
+# import {
+#   to = aws_route53_zone.private
+#   id = "Z04931192K8IHLCR9QGHU"
+# }
+
+# import {
+#   to = aws_route53_record.private_ns
+#   id = "Z04931192K8IHLCR9QGHU_dev.sdc.dot.gov_NS"
+# }
+
+# import {
+#   to = aws_route53_record.private_soa
+#   id = "Z04931192K8IHLCR9QGHU_dev.sdc.dot.gov_SOA"
+# }
+
+# import {
+#   to = module.gitlab.aws_route53_record.gitlab
+#   id = "Z04931192K8IHLCR9QGHU_gitlab.dev.sdc.dot.gov_A"
 # }
 
 # import {
