f9cb1d6e9616955c47308251e8572d45db11aeb9
williamrsharp <69211914+williamrsharp@users.noreply.github.com>
2024-09-03 18:43:06 +0000

    added logger to auto-stop

--- terraform/auto_stop/auto-stop.py
+++ terraform/auto_stop/auto-stop.py
@@ -4,6 +4,9 @@ import os
 from croniter import croniter
 import pytz
 import ctypes
+import psutil
+import time
+import logging
 
 # Set region
 boto3.setup_default_session(region_name='us-east-1')
@@ -77,18 +80,68 @@ def shutdown_computer():
     """
     Shuts down the computer.
     """
-    print("SHUTTING DOWN")
-    os.system('shutdown /s /t 1')
+    logging.info('shutting down')
+    time.sleep(3)
+    #os.system('shutdown /s /t 1')
+
+def script_running():
+    processes = [
+        p.cmdline() for p in psutil.process_iter() 
+        if (p.name().lower() in ['python.exe'] and 'auto-stop.py' not in p.cmdline()[1])
+        or (p.name().lower() in ['r', 'rscript'])
+    ]    
+    
+    if len(processes) > 0:
+        logging.info('a script is running on the computer')
+
+    return len(processes)
+
+def get_idle_time():
+    if os.name == 'nt':  # Windows
+        from ctypes import Structure, windll, c_uint, sizeof, byref
+        
+        class LASTINPUTINFO(Structure):
+            _fields_ = [("cbSize", c_uint), ("dwTime", c_uint)]
+
+        lii = LASTINPUTINFO()
+        lii.cbSize = sizeof(LASTINPUTINFO)
+        windll.user32.GetLastInputInfo(byref(lii))
+        millis = windll.kernel32.GetTickCount() - lii.dwTime
+        return millis / 1000.0  # Convert to seconds
+    else:
+        raise NotImplementedError("Idle time detection is not implemented for this OS")
+
+# Function to track idle time
+def track_idle_time():
+    idle_threshold = 3600  # 1 hour in seconds
+    idle_time = get_idle_time()
+
+    # Check if idle time is greater than or equal to the threshold
+    if idle_time >= idle_threshold:
+        logging.info("Computer has been idle for 1 hour.")
+        return True
+    else:
+        logging.info(f"Idle time: {idle_time/60:.2f} minutes")
+        return False
 
 if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file = os.path.join(script_dir, 'auto-stop-logs.log')
+    logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,  # Set the logging level
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    logging.info('auto-stop.py called by scheduled task')
     maintenance_windows = get_maintenance_windows()
     within_window = False
     for cron_expression, duration, timezone in maintenance_windows:
         if is_within_maintenance_window(cron_expression, duration, timezone):
             within_window = True
+            logging.info('within maintenance window')
             break
-    
-    if within_window:
+    script_running = script_running()
+    if within_window or not script_running == 0 and not track_idle_time():
         print("Within maintenance window. No action taken.")
     else:
         print("Not within maintenance window. Shutting down the computer.")
