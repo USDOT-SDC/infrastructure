bfce71e9aeacb9ee0fb8ca9678d20d803ce39138
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2026-07-23 20:49:04 -0400

    add endpoints

--- terraform/vpc/endpoints.tf
+++ terraform/vpc/endpoints.tf
@@ -93,13 +93,10 @@ resource "aws_vpc_endpoint" "ssm_interface" {
   vpc_id            = var.common.network.vpc.id
   vpc_endpoint_type = "Interface"
   service_name      = "com.amazonaws.${var.common.region}.ssm"
-  subnet_ids = concat(
-    [
-      aws_subnet.support.id,
-      aws_subnet.researcher.id
-    ],
-    aws_subnet.infrastructure[*].id
-  )
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
   tags = {
     "Name" = "SSM Interface"
   }
@@ -128,13 +125,10 @@ resource "aws_vpc_endpoint" "sqs_interface" {
   vpc_id            = var.common.network.vpc.id
   vpc_endpoint_type = "Interface"
   service_name      = "com.amazonaws.${var.common.region}.sqs"
-  subnet_ids = concat(
-    [
-      aws_subnet.support.id,
-      aws_subnet.researcher.id
-    ],
-    aws_subnet.infrastructure[*].id
-  )
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
   tags = {
     "Name" = "SQS Interface"
   }
@@ -158,19 +152,144 @@ resource "aws_vpc_endpoint_policy" "sqs_interface" {
   )
 }
 
-# STS Interface
-resource "aws_vpc_endpoint" "sts_interface" {
+# ECR API Interface
+resource "aws_vpc_endpoint" "ecr_api_interface" {
   vpc_id            = var.common.network.vpc.id
   vpc_endpoint_type = "Interface"
-  service_name      = "com.amazonaws.${var.common.region}.sts"
-  security_group_ids = [ var.common.network.default_security_group.id ]
-  subnet_ids = concat(
-    [
-      aws_subnet.support.id,
-      aws_subnet.researcher.id
-    ],
-    aws_subnet.infrastructure[*].id
+  service_name      = "com.amazonaws.${var.common.region}.ecr.api"
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
+  tags = {
+    "Name" = "ECR API Interface"
+  }
+}
+
+resource "aws_vpc_endpoint_policy" "ecr_api_interface" {
+  vpc_endpoint_id = aws_vpc_endpoint.ecr_api_interface.id
+  policy = jsonencode(
+    {
+      "Version" : "2008-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowAll",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "*",
+          "Resource" : "*"
+        }
+      ]
+    }
   )
+}
+
+# ECR DKR Interface
+resource "aws_vpc_endpoint" "ecr_dkr_interface" {
+  vpc_id            = var.common.network.vpc.id
+  vpc_endpoint_type = "Interface"
+  service_name      = "com.amazonaws.${var.common.region}.ecr.dkr"
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
+  tags = {
+    "Name" = "ECR DKR Interface"
+  }
+}
+
+resource "aws_vpc_endpoint_policy" "ecr_dkr_interface" {
+  vpc_endpoint_id = aws_vpc_endpoint.ecr_dkr_interface.id
+  policy = jsonencode(
+    {
+      "Version" : "2008-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowAll",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "*",
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
+
+# Secrets Manager Interface
+resource "aws_vpc_endpoint" "secretsmanager_interface" {
+  vpc_id            = var.common.network.vpc.id
+  vpc_endpoint_type = "Interface"
+  service_name      = "com.amazonaws.${var.common.region}.secretsmanager"
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
+  tags = {
+    "Name" = "Secrets Manager Interface"
+  }
+}
+
+resource "aws_vpc_endpoint_policy" "secretsmanager_interface" {
+  vpc_endpoint_id = aws_vpc_endpoint.secretsmanager_interface.id
+  policy = jsonencode(
+    {
+      "Version" : "2008-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowAll",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "*",
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
+
+# CloudWatch Logs Interface
+resource "aws_vpc_endpoint" "logs_interface" {
+  vpc_id            = var.common.network.vpc.id
+  vpc_endpoint_type = "Interface"
+  service_name      = "com.amazonaws.${var.common.region}.logs"
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
+  tags = {
+    "Name" = "CloudWatch Logs Interface"
+  }
+}
+
+resource "aws_vpc_endpoint_policy" "logs_interface" {
+  vpc_endpoint_id = aws_vpc_endpoint.logs_interface.id
+  policy = jsonencode(
+    {
+      "Version" : "2008-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowAll",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "*",
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
+
+# STS Interface
+resource "aws_vpc_endpoint" "sts_interface" {
+  vpc_id             = var.common.network.vpc.id
+  vpc_endpoint_type  = "Interface"
+  service_name       = "com.amazonaws.${var.common.region}.sts"
+  security_group_ids = [var.common.network.default_security_group.id]
+  subnet_ids = [
+    aws_subnet.support.id,
+    aws_subnet.researcher.id
+  ]
   tags = {
     "Name" = "STS Interface"
   }
